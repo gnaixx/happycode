a55dcaf867bf79343c71442f89ff8d85947dcb5b
凸一_一凸 <990828785@qq.com>
2021-04-01 13:22:40 +0800

    done 88

---
+++ python3/two-pointers/easy/88.合并两个有序数组.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode.cn id=88 lang=python3
+#
+# [88] 合并两个有序数组
+#
+
+# @lc code=start
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        while m>0 and n>0:
+            if nums1[m-1] > nums2[n-1]:
+                nums1[m+n-1] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[m+n-1] = nums2[n-1]
+                n -= 1
+        nums1[:n] = nums2[:n] # m先为0，剩余nums2全赋值
+# @lc code=end
+
