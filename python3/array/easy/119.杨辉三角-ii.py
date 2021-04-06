e268704f02cd13f071b1583fe3881316df22bdd3
凸一_一凸 <990828785@qq.com>
2021-04-07 00:15:36 +0800

    done 119

---
+++ python3/array/easy/119.杨辉三角-ii.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode.cn id=119 lang=python3
+#
+# [119] 杨辉三角 II
+#
+
+# @lc code=start
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        row = [1] * (rowIndex + 1)
+        for i in range(1, rowIndex):
+            row[i] *= int(row[i-1] * (rowIndex - i + 1) / i)
+        return row
+# @lc code=end
+
