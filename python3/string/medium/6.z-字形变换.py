5b95001f3efd345ec2e65678d6a84476cd5e9f24
凸一_一凸 <990828785@qq.com>
2021-04-09 23:44:01 +0800

    done 6

---
+++ python3/string/medium/6.z-字形变换.py
@@ -0,0 +1,35 @@
+#
+# @lc app=leetcode.cn id=6 lang=python3
+#
+# [6] Z 字形变换
+#
+
+# @lc code=start
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        def format(rows):
+            s = ''
+            for i in range(len(rows)):
+                for j in range(len(rows[i])):
+                    if rows[i][j]:
+                        s += rows[i][j]
+            return s
+
+        if numRows==1:
+            return s
+            
+        rows = [[] for i in range(numRows)]
+        index = 0
+        for j in range(1000):
+            for i in range(numRows):
+                if j%(numRows-1)==0 or (i+j)%(numRows-1)==0:
+                    rows[i].append(s[index])
+                    index += 1
+                else:
+                    rows[i].append(None)
+                if index >= len(s):
+                    return format(rows)
+
+# @lc code=end
+
+Solution().convert('A', 1)
