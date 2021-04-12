41c4d75b4ca90a221f32de3bc545c074457e472c
凸一_一凸 <990828785@qq.com>
2021-04-12 23:44:04 +0800

    done 136

---
+++ python3/bit-manipulation/easy/136.只出现一次的数字.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode.cn id=136 lang=python3
+#
+# [136] 只出现一次的数字
+#
+
+# @lc code=start
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        x = 0
+        for i in nums:
+            x ^= i
+        return x
+# @lc code=end
+
