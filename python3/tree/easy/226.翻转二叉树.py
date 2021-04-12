594ab9760f59a23b00ebc86f81ff0996e9b6a856
凸一_一凸 <990828785@qq.com>
2021-04-12 23:55:29 +0800

    done 226

---
+++ python3/tree/easy/226.翻转二叉树.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode.cn id=226 lang=python3
+#
+# [226] 翻转二叉树
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return root
+        root.left, root.right = root.right, root.left
+        if root.left:
+            self.invertTree(root.left)
+        if root.right:
+            self.invertTree(root.right)
+        return root
+# @lc code=end
+
