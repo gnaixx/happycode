cdace17b8ea447e1aeaba4a5602a76490883593c
凸一_一凸 <990828785@qq.com>
2021-04-17 16:48:08 +0800

    done 108

--- python3/tree/easy/108.将有序数组转换为二叉搜索树.py
+++ python3/tree/easy/108.将有序数组转换为二叉搜索树.py
@@ -4,6 +4,9 @@
 # [108] 将有序数组转换为二叉搜索树
 #
 
+from typing import List
+from python3.aatool.TreeT import TreeNode
+
 # @lc code=start
 # Definition for a binary tree node.
 # class TreeNode:
@@ -12,6 +15,16 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def createNode(self, nums: List[int], left: int, right: int) -> TreeNode:
+        if left > right:
+            return None
+        mid = (left+right) // 2
+        leftNode = self.createNode(nums, left, mid-1)
+        rightNode = self.createNode(nums, mid+1, right)
+        return TreeNode(nums[mid], leftNode, rightNode)
+
     def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        return self.createNode(nums, 0, len(nums)-1)
 # @lc code=end
 
+Solution().sortedArrayToBST([-10,-3,0,5,9])
