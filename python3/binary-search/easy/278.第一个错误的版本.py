fc02e7d82507416ac1fb401bc3a150b9292ad071
凸一_一凸 <990828785@qq.com>
2021-04-20 23:50:40 +0800

    done 278

---
+++ python3/binary-search/easy/278.第一个错误的版本.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode.cn id=278 lang=python3
+#
+# [278] 第一个错误的版本
+#
+
+# @lc code=start
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return an integer
+# def isBadVersion(version):
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        l, r = 1, n
+        while l < r:
+            mid = int((l + r) / 2)
+            if isBadVersion(mid):
+                r = mid
+            else:
+                l = mid + 1
+        return l
+# @lc code=end
+
