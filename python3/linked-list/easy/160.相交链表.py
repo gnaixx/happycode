eb220ca9146b2d636825b37b73530513be61fa1c
凸一_一凸 <990828785@qq.com>
2021-05-09 11:07:54 +0800

    done 160

--- python3/linked-list/easy/160.相交链表.py
+++ python3/linked-list/easy/160.相交链表.py
@@ -3,6 +3,7 @@
 #
 # [160] 相交链表
 #
+from python3.aatool.LinkT import ListNode, createSLink
 
 # @lc code=start
 # Definition for singly-linked list.
@@ -12,18 +13,29 @@
 #         self.next = None
 
 class Solution:
-    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
-        if not headA or not headB: return None
-        # 必须set否则会超时
-        tmpA, tmpB, nodeList = headA, headB, set() 
-        while tmpA:
-            nodeList.add(tmpA)
-            tmpA = tmpA.next
+    # def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+    #     if not headA or not headB: return None
+    #     # 必须set否则会超时
+    #     tmpA, tmpB, nodeList = headA, headB, set() 
+    #     while tmpA:
+    #         nodeList.add(tmpA)
+    #         tmpA = tmpA.next
         
-        while tmpB:
-            if tmpB in nodeList:
-                return tmpB
-            tmpB = tmpB.next
-        return None
+    #     while tmpB:
+    #         if tmpB in nodeList:
+    #             return tmpB
+    #         tmpB = tmpB.next
+    #     return None
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        if headA is None or headB is None:
+            return None
+        pa = headA
+        pb = headB
+        # 非相交最后会一起走到 None
+        while pa is not pb:
+            pa = headB if pa is None else pa.next
+            pb = headA if pb is None else pb.next
+        return pa 
 # @lc code=end
 
+Solution().getIntersectionNode(createSLink([1, 2, 3]), createSLink([4, 5]))
