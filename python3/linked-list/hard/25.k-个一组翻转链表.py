8947e8e18640626e25e40c66fe6b60429d1fc8c0
凸一_一凸 <990828785@qq.com>
2021-05-22 16:57:29 +0800

    done 25

---
+++ python3/linked-list/hard/25.k-个一组翻转链表.py
@@ -0,0 +1,45 @@
+#
+# @lc app=leetcode.cn id=25 lang=python3
+#
+# [25] K 个一组翻转链表
+#
+
+from typing import List
+from python3.aatool.LinkT import ListNode, createSLink
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverse(self, preNode: ListNode, head: ListNode, k: int) -> ListNode:
+        temp, stack = head, []
+        for i in range(k):
+            if temp:
+                stack.append(temp)
+                temp = temp.next
+            else:
+                break
+        if len(stack) < k:
+            return None
+        for i in range(k-1, -1, -1):
+            preNode.next = stack[i]
+            preNode = preNode.next
+            if i == k-1:
+                lstNode = stack[i].next
+            if i == 0:
+                stack[i].next = lstNode
+        return preNode
+
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        dummyNode = ListNode(val=0, next=head)
+        preNode = dummyNode
+        while preNode:
+            preNode = self.reverse(preNode, head, k)
+            head = preNode.next if preNode else None
+        return dummyNode.next
+# @lc code=end
+
+Solution().reverseKGroup(createSLink([1,2,3,4,5]), 1)
--- zsum.sh
+++ zsum.sh
@@ -1,6 +1,10 @@
 echo 'pthon3:'
 # find python3 -type f -name "*.*.py" | cut -f3 -d '.'| sort | uniq -c -i
 find python3 -type f -name "*.*.py" | cut -f3 -d '/' | sort | uniq -c
-easy=$(find python3 -type f -name "*.*.py" | cut -f3 -d '/' | sort | uniq -c | grep easy | cut -f3 -d ' ')
-medium=$(find python3 -type f -name "*.*.py" | cut -f3 -d '/' | sort | uniq -c | grep medium | cut -f3 -d ' ')
-echo 'total: '$[$easy+$medium]
+
+easy=$(find python3 -type f -name "*.*.py" | cut -f3 -d '/' | sort | uniq -c | grep easy | tr -cd '[0-9]')
+medium=$(find python3 -type f -name "*.*.py" | cut -f3 -d '/' | sort | uniq -c | grep medium | tr -cd '[0-9]')
+hard=$(find python3 -type f -name "*.*.py" | cut -f3 -d '/' | sort | uniq -c | grep hard | tr -cd '[0-9]')
+echo 'total: '$(( $easy+$medium+$hard ))
+
+   
