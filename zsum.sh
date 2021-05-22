431dbccf1d74009089a495d2f82dae9c768bf353
凸一_一凸 <990828785@qq.com>
2021-05-22 10:20:46 +0800

    update zsum

--- zsum.sh
+++ zsum.sh
@@ -1,3 +1,6 @@
 echo 'pthon3:'
 # find python3 -type f -name "*.*.py" | cut -f3 -d '.'| sort | uniq -c -i
 find python3 -type f -name "*.*.py" | cut -f3 -d '/' | sort | uniq -c
+easy=$(find python3 -type f -name "*.*.py" | cut -f3 -d '/' | sort | uniq -c | grep easy | cut -f3 -d ' ')
+medium=$(find python3 -type f -name "*.*.py" | cut -f3 -d '/' | sort | uniq -c | grep medium | cut -f3 -d ' ')
+echo 'total: '$[$easy+$medium]
