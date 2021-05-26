1baf996c07e586755eb3b3f7936376250a045f06
凸一_一凸 <gnaixog@gmail.com>
2021-05-26 16:17:32 +0800

    done 225

---
+++ python3/stack/easy/225.用队列实现栈.py
@@ -0,0 +1,49 @@
+#
+# @lc app=leetcode.cn id=225 lang=python3
+#
+# [225] 用队列实现栈
+#
+
+# @lc code=start
+class MyStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x onto stack.
+        """
+        self.stack.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element on top of the stack and returns that element.
+        """
+        return self.stack.pop()
+
+    def top(self) -> int:
+        """
+        Get the top element.
+        """
+        return self.stack[-1]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the stack is empty.
+        """
+        return True if not self.stack or len(self.stack)==0 else False
+
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
+# @lc code=end
+
