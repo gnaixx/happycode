049abf12a460440f0065a4098dd7034b2066e34b
凸一_一凸 <gnaixog@gmail.com>
2021-05-27 20:17:35 +0800

    done easy 258

---
+++ python3/math/easy/258.各位相加.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode.cn id=258 lang=python3
+#
+# [258] 各位相加
+#
+
+# @lc code=start
+class Solution:
+    def addDigits(self, num: int) -> int:
+        return num if num == 0 else num % 9 or 9
+# @lc code=end
+
