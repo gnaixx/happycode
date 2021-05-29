73d10d097a3692e3ecc4da2676c4d41b4896cb4f
凸一_一凸 <gnaixog@gmail.com>
2021-05-29 23:58:10 +0800

    do easy 268

---
+++ python3/bit-manipulation/easy/268.丢失的数字.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode.cn id=268 lang=python3
+#
+# [268] 丢失的数字
+#
+
+# @lc code=start
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums) + 1
+        for i in nums:
+            n 
+# @lc code=end
+
