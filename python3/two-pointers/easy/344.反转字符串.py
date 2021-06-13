a752412d1416582a6f6c98fca3bf6316e870a66b
凸一_一凸 <990828785@qq.com>
2021-06-13 16:42:18 +0800

    done easy 344

---
+++ python3/two-pointers/easy/344.反转字符串.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode.cn id=344 lang=python3
+#
+# [344] 反转字符串
+#
+
+# @lc code=start
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        l, r = 0, len(s)-1
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l, r = l+1, r-1
+# @lc code=end
+
